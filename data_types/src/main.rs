36d84d716c0cedfa4bd047d3ef52ee6382092746
Andre <andre.colin@epitech.eu>
2025-06-23 14:19:00 +0200

    editing exercise

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,12 +1,12 @@
-mod integers_range;
-use crate::integers_range::fits_in_type;
-mod compilation_error;
-use crate::compilation_error::compilation_error;
+// mod integers_range;
+// use crate::integers_range::fits_in_type;
+// mod compilation_error;
+// use crate::compilation_error::compilation_error;
 
 
 fn main() {
     println!("Hello, world!");
 
-    println!("{}", fits_in_type(800, "i8"));
-    println!("{}", fits_in_type(100, "i8"));
+    // println!("{}", fits_in_type(800, "i8"));
+    // println!("{}", fits_in_type(100, "i8"));
 }
